3faf0d732ec9b0cd1ea1c3341170cb6a21825983
frankmata17 <frank.mata17@gmail.com>
2024-06-11 20:12:28 -0600

    first commit

--- server.js
+++ server.js
@@ -6,14 +6,33 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") 
+
+/* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(require("./routes/static"))
+
+//Index route
+// localhost:5500
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+// localhost:5500/test
+app.get("/test", function(req, res){
+  res.send("Hello, World!");
+})
 
 /* ***********************
  * Local Server Information
